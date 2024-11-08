1cabf5d2a1698d703724b410ce48a718997bc17a
Levi-F06 <levi.flanagan06@gmail.com>
2024-11-08 11:11:05 +0000

    :sparkles: Create click interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
+fn clear_screen() {
+    std::process::Command::new("tput")
+        .arg("clear")
+        .status()
+        .expect("Failed to clear screen");
+}
+
+fn click(mut player_clicks: u32) {
+    loop {
+        clear_screen();
+
+        let mut command = String::new();
+
+        println!("____________________");
+        println!("\n\n -> Coins: {player_clicks}");
+        println!("\n\nCommands:");
+        println!("Enter key -> click");
+        println!("S -> Shop");
+        println!("E -> Exit");
+        println!("R -> Reset");
+        println!("\n\n____________________");
+
+        std::io::stdin()
+            .read_line(&mut command)
+            .expect("Failed to read line");
+
+        if command == "\n" {
+            player_clicks = player_clicks + 1;
+            continue;
+        }
+
+        command = command.trim().to_lowercase();
+
+        if command == "e" || command == "exit" {
+            break;
+        }
+        if command == "s" || command == "shop" {
+            break shop();
+        }
+        if command == "r" || command == "reset" {
+            break click(0);
+        }
+    }
+}
+
+fn shop() {
+    println!("WIP");
+}
+
 fn main() {
-    println!("Hello, world!");
+    click(0);
 }
